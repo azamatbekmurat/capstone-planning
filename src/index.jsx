a4d3205e55589f449a82674a5bd2cd83cc6256d4
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-07-20 16:46:26 -0700

    update rendering on index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-import { AppContainer } from 'react-hot-loader'
-import { HashRouter } from 'react-router-dom'
-import { createStore } from 'redux'
-import ticketListReducer from './reducers/ticket-list-reducer'
-import { Provider } from 'react-redux'
-import rootReducer from './reducers/index'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+import persistDataLocally from './middleware/persist-data-locally';
 
-const store = createStore(rootReducer)
+let retrievedState;
+try {
+  retrievedState = localStorage.getItem('reduxStore');
+  if (retrievedState === null){
+    retrievedState = {};
+  }
+  retrievedState = JSON.parse(retrievedState);
+} catch (err){
+  retrievedState = {};
+}
+
+const store = createStore(rootReducer, retrievedState, applyMiddleware(persistDataLocally));
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
-)
-
+);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <HashRouter>
-        <Provider store={store}>
-          <Component/>
-        </Provider>
-      </HashRouter>
-    </AppContainer>,
+    <HashRouter>
+      <Provider store={store}>
+        <Component/>
+      </Provider>
+    </HashRouter>,
     document.getElementById('react-app-root')
-  )
-}
+  );
+};
 
-render(App)
+render(App);
 
 /*eslint-disable */
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    render(App)
+    render(App);
   });
 }
 /*eslint-enable */
